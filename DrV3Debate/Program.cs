903f0432fe1f5af6eb5229355475b110402d4ca8
morgana <89588301+morgana-x@users.noreply.github.com>
2025-02-09 02:00:10 +1100

    Add Batch Processing

--- DrV3Debate/Program.cs
+++ DrV3Debate/Program.cs
@@ -1,26 +1,54 @@
 ﻿using DrV3Debate;
 public partial class Program
 {
-    public static void Main(string[] args)
+    private static void ProcessFile(string filePath, string outPath = "")
     {
-        string filePath = "";
-        if (args.Length > 0 )
-            filePath = args[0];
-        else
-        {
-            Console.WriteLine("Drag and drop file to extract");
-            filePath = Console.ReadLine().Replace("\"", "");
-        }
         if (filePath.EndsWith(".txt"))
         {
             Console.WriteLine("Repacking dat file...");
-            V3DebateDat.Repack(filePath); // UNFINISHED, PADDING NOT IMPLEMENTED FOR VOICE EFFECT STRINGS
+            V3DebateDat.Repack(filePath, outPath.Replace(".txt", ".dat"));
             Console.WriteLine("Repacked dat file...");
             return;
         }
         Console.WriteLine("Exporting Dat to TXT...");
         V3DebateDat dat = new(filePath);
-        dat.Extract(filePath.Replace(".dat", ".txt"));
+        dat.Extract(outPath == "" ? filePath.Replace(".dat", ".txt") : outPath.Replace(".dat", ".txt"));
         Console.WriteLine("Done!");
+    }
+    private static void Loop(string filePath, string outPath ="") 
+    {
+        if (Directory.Exists(filePath))
+        {
+            if (outPath != "")
+                Directory.CreateDirectory(outPath);
+            foreach (var subFile in Directory.GetFiles(filePath))
+                ProcessFile(subFile, outPath != "" ? Path.Combine(outPath, Path.GetFileName(subFile)) : "");
+            return;
+        }
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine($"File {filePath} does not exist!");
+            return;
+        }
+        ProcessFile(filePath, outPath);
+    }
+
+    public static void Main(string[] args)
+    {
+        if (args.Length > 0)
+        {
+            if (args.Length > 1)
+            {
+                Loop(args[0], args[1]);
+                return;
+            }
+            Loop(args[0]);
+            return;
+        }
+        while (true)
+        {
+            Console.WriteLine("Drag and drop file to extract");
+            Loop(Console.ReadLine().Replace("\"", ""));
+        }
     }
 }
